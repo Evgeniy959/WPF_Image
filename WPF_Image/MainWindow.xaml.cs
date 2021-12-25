e37c861a8136a2213d65b6a3df65e4d540c47e12
Starinin Andrey <starininandrey@gmail.com>
2021-12-25 12:58:50 +0300

    Done

--- WPF_Image/MainWindow.xaml.cs
+++ WPF_Image/MainWindow.xaml.cs
@@ -1,28 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace WPF_Image
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_OpenImage_OnClick(object sender, RoutedEventArgs e)
+        {
+            var path = new Uri(OpenFileDialog());
+            Image.Source = new BitmapImage(path);
+        }
+
+        private string OpenFileDialog()
+        {
+            var openFileDialog = new OpenFileDialog();
+            
+            return openFileDialog.ShowDialog() == true ? openFileDialog.FileName : string.Empty;
+        }
+
+        private void Button_ChangeColor(object sender, RoutedEventArgs e)
+        {
+            var name = (sender as Button)?.Name;
+
+            Canvas.DefaultDrawingAttributes.Color = name switch
+            {
+                "Button_ColorBlack" => Colors.Black,
+                "Button_ColorRed" => Colors.Red,
+                _ => Canvas.DefaultDrawingAttributes.Color
+            };
         }
     }
 }
